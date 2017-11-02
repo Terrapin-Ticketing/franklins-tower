76feeffd5001845a2158f2a7a82c5d3e146ad89a
Michael Reeder <reeder@knoesis.org>
2017-11-02 16:12:28 -0400

    made tests much better

--- test/event-manager.js
+++ test/event-manager.js
@@ -23,6 +23,7 @@ function guidGenerator() {
 }
 
 let wei = 1000000000000000000;
+let TICKET_QTY = 10;
 
 contract('EventManager', function(accounts) {
   before(function() {
@@ -45,13 +46,33 @@ contract('EventManager', function(accounts) {
       .then(() => terrapin.getEvents.call())
       .then((eventAddresses) => {
         let eventInstance = Event.at(eventAddresses[0]);
-        return eventInstance.printTicket(price, {
-          from: accounts[1],
-          gas: 4700000
+        return pasync.eachSeries(new Array(TICKET_QTY), () => {
+          return eventInstance.printTicket(price, {
+            from: accounts[1],
+            gas: 4700000
+          });
         }).then(() => eventInstance);
       })
       .then((eventInstance) => {
         this.eventInstance = eventInstance;
+      });
+  });
+
+  before(function() {
+    this.nextTicket = 0;
+    let ticketInstances = [];
+    return this.eventInstance.getTickets.call()
+      .then((ticketAddresses) => {
+        return pasync.eachSeries(ticketAddresses, (ticketAddress) => {
+          ticketInstances.push(Ticket.at(ticketAddress));
+        });
+      })
+      .then(() => {
+        this.getTicketInstance = () => {
+          return new Promise((resolve) => {
+            resolve(ticketInstances[this.nextTicket++]);
+          });
+        };
       });
   });
 
@@ -64,10 +85,8 @@ contract('EventManager', function(accounts) {
 
   it('should buy a ticket', function() {
     let ticketInstance, owner;
-    let eventInstance = this.eventInstance;
 
-    return eventInstance.getTickets.call()
-      .then((ticketAddresses) => Ticket.at(ticketAddresses[0]))
+    return this.getTicketInstance()
       .then((_ticketInstance) => {
         ticketInstance = _ticketInstance;
         return ticketInstance.buyTicket({
@@ -101,9 +120,8 @@ contract('EventManager', function(accounts) {
 
   it('should call masterBuy from master account', function() {
     let ticketInstance;
-    let eventInstance = this.eventInstance;
-    return eventInstance.getTickets.call()
-      .then((ticketAddresses) => Ticket.at(ticketAddresses[0]))
+
+    return this.getTicketInstance()
       .then((_ticketInstance) => ticketInstance = _ticketInstance)
       .then(() => {
         return ticketInstance.masterBuy(accounts[1], {
@@ -117,6 +135,51 @@ contract('EventManager', function(accounts) {
           assert(_owner === accounts[1]);
           assert(_isForSale === false);
         });
+      });
+  });
+
+  it('should set is for sale', function() {
+    let ticketInstance, owner;
+
+    return this.getTicketInstance()
+      .then((_ticketInstance) => {
+        ticketInstance = _ticketInstance;
+        return ticketInstance.buyTicket({
+          from: accounts[2],
+          gas: 4700000,
+          value: wei * 20
+        });
+      })
+      .then(() => {
+        return ticketInstance.owner.call().then((_owner) => {
+          owner = _owner;
+        });
+      })
+      .then(() => {
+        let boughtEvent = ticketInstance.Bought();
+        return new Promise((resolve, reject) => {
+          boughtEvent.watch((err, result) => {
+            if (err) return reject(err);
+            assert(Boolean(result.args.status) === true);
+            resolve();
+            boughtEvent.stopWatching();
+          });
+        });
+      })
+      .then(() => {
+        return ticketInstance.owner.call().then((newOwner) => {
+          assert(owner !== newOwner);
+        });
+      })
+      .then(() => {
+        return ticketInstance.setIsForSale(true, {
+          from: accounts[2],
+          gas: 4700000
+        });
+      })
+      .then(() => ticketInstance.isForSale.call())
+      .then((isForSale) => {
+        assert(isForSale);
       });
   });
 
